3f6bd64ca0a00c6c855a9832c098bd33435c5fd1
gulnar-code <memmedovagulnar757@gmail.com>
2025-04-07 14:37:58 +0400

    Update lab 2 gulu.py

--- lab 2 gulu.py
+++ lab 2 gulu.py
@@ -1,8 +1,12 @@
-import random
-list= []
+import random 
+list=[]
 for i in range(0,5):
-    number=random.randint(1,10)
+    number=random.randint(-6,10)
     list.append(number)
     i=i+1
-
 print(list)
+s=0
+for i in list:
+    if i>0:
+        s=s+i
+print(s)
